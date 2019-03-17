d1e385e86634d4ef57cf8be591dba94c95060358
Vinícius Orsi Valente <viniciusov@hotmail.com>
2019-03-16 22:48:51 -0300

    New checks after shutdown warning

--- plex-shutdown.sh
+++ plex-shutdown.sh
@@ -3,9 +3,9 @@
 run_test () {
     processes=$(ps -aux | grep -c plex)
     if [ "$processes" -gt 6 ]; then
-        return 0
+        return 0 #if plex is running
     else
-        return 1
+        return 1 #if plex is not running
     fi
 }
 
@@ -14,17 +14,29 @@ waiting () {
     run_test
 }
 
-waiting
+while : ; do
+	waiting
+	
+	while run_test ; do #if plex is running
+	    waiting
+	done
+	
+	shut_time=$(date --date='10 minutes' +"%T")
+	notify-send -t 600000 -i "/usr/share/plex-shutdown/plex.svg" "WARNING:
+	Plex is not running.
+	Shutting down in 10 minutes (scheduled for $shut_time).
+	Open the terminal and type shutdown -c to cancel."
+	
+	sudo /sbin/shutdown -h +10
 
-while run_test 
-do
-    waiting
+	while ! run_test ; do #if plex is not running
+		sleep 1m
+		run_test
+		if run_test ; then #if plex is running
+			sudo /sbin/shutdown -c
+			notify-send -t 600000 -i "/usr/share/plex-shutdown/plex.svg" "Shutdown Cancelled."
+			break
+		fi
+	done 
 done
 
-shut_time=$(date --date='10 minutes' +"%T")
-notify-send -t 600000 -i "/usr/share/plex-shutdown/plex.svg" "WARNING:
-Plex is not running.
-Shutting down in 10 minutes (scheduled for $shut_time).
-Open the terminal and type shutdown -c to cancel."
-
-sudo /sbin/shutdown -h +10
