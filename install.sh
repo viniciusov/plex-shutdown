eb48c4caf189ed00c2369bf4376348bf124563a7
Vinícius Orsi Valente <viniciusov@hotmail.com>
2019-03-20 10:00:27 -0300

    Installer updated

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 echo 'Installing...'
 
+mkdir ~/.config/autostart
+cp plex-shutdown.desktop ~/.config/autostart/
+
 user=$USER
 sudo bash -c 'echo "'$user' ALL=(ALL) NOPASSWD: /sbin/shutdown" > /etc/sudoers.d/shutdown'
 
-chmod 777 *.sh
 sudo mkdir /usr/share/plex-shutdown
 sudo cp plex-shutdown.sh /usr/share/plex-shutdown/
 sudo cp plex.svg /usr/share/plex-shutdown/
-sudo cp plex-shutdown.desktop ~/.config/autostart/
 
 echo 'Done!'
