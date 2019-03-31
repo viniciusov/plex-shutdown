f7443a3c23418e27a14929e938218334c5f02b8a
Vinícius Orsi Valente <viniciusov@hotmail.com>
2019-03-31 14:47:40 -0300

    Monitor plex server even if it is not transcoding

--- plex-shutdown.sh
+++ plex-shutdown.sh
@@ -2,11 +2,11 @@
 
 while : ; do
 	counter=0
-	while [ $(ps -aux | grep -c 'Plex Transcoder') -le 1 ] ; do #if plex is not running
+	while [ $(ps -aux | grep -c 'plexmediaserver') -le 6 ] ; do #if plex is not running
 		sleep 1m
 		((counter++))
 		if [ $counter -lt 50 ] ; then
-			if [ $(ps -aux | grep -c 'Plex Transcoder') -gt 1 ] ; then #if plex is running
+			if [ $(ps -aux | grep -c 'plexmediaserver') -gt 6 ] ; then #if plex is running
 				counter=0
 			fi
 		else
@@ -22,9 +22,9 @@ Type shutdown -c in a terminal to cancel."
 	
 	sudo /sbin/shutdown -h +10
 
-	while [ $(ps -aux | grep -c 'Plex Transcoder') -le 1 ] ; do #if plex is not running
+	while [ $(ps -aux | grep -c 'plexmediaserver') -le 6 ] ; do #if plex is not running
 		sleep 1m
-		if [ $(ps -aux | grep -c 'Plex Transcoder') -gt 1 ] || ! ps -C shutdown > /dev/null ; then #if plex is running or shutdown cancelled
+		if [ $(ps -aux | grep -c 'plexmediaserver') -gt 6 ] || ! ps -C shutdown > /dev/null ; then #if plex is running or shutdown cancelled
 			if ps -C shutdown > /dev/null ; then #if shutdown is still pending
   				sudo /sbin/shutdown -c
 			fi
