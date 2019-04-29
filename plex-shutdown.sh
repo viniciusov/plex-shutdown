d48824fc6f64e20bb09e8f54f987533cbc0c546b
Vinícius Orsi Valente <viniciusov@hotmail.com>
2019-04-29 09:23:33 -0300

    Now supports changes on initial processes number

--- plex-shutdown.sh
+++ plex-shutdown.sh
@@ -5,18 +5,24 @@ log_path=~/.config/plex-shutdown/log
 echo --------------------------------------- >> $log_path
 echo [$(date)] 'Started' >> $log_path
 sleep 5s #wait for system initialization
-proc_num=$(ps -aux | grep -c 'Plex Transcoder') #number of plex processes right after startup
+proc_num=$(ps -aux | grep -c 'plexmediaserver') #number of plex processes right after startup
 echo [$(date)] 'Initial Plex Processes:' $proc_num >> $log_path
 
 while : ; do
 	counter=0
 	pending=false
-	while [ $(ps -aux | grep -c 'Plex Transcoder') -le $proc_num ] ; do #if plex is not running
+	while [ $(ps -aux | grep -c 'plexmediaserver') -le $proc_num ] ; do #if plex is not running
+		if [ $(ps -aux | grep -c 'plexmediaserver') -lt $proc_num ] ; then
+			proc_num=$(ps -aux | grep -c 'plexmediaserver')
+			echo [$(date)] 'Counter:' $counter >> $log_path
+			echo [$(date)] 'Plex Processes:' $(ps -aux | grep -c 'plexmediaserver') >> $log_path
+			echo [$(date)] 'Initial Plex Processes changed to' $proc_num >> $log_path
+		fi	
 		sleep 1m
 		((counter++))
-		if [ $counter -lt 50 ] && [ $(ps -aux | grep -c 'Plex Transcoder') -gt $proc_num ] ; then #if plex start
+		if [ $counter -lt 50 ] && [ $(ps -aux | grep -c 'plexmediaserver') -gt $proc_num ] ; then #if plex start
 			echo [$(date)] 'Counter:' $counter >> $log_path
-			echo [$(date)] 'Plex Processes:' $(ps -aux | grep -c 'Plex Transcoder') >> $log_path
+			echo [$(date)] 'Plex Processes:' $(ps -aux | grep -c 'plexmediaserver') >> $log_path
 			echo [$(date)] 'Plex activity detected. Restarting timer...' >> $log_path
 			break
 		elif [ $counter -eq 50 ] ; then #if not running until first 50 minutes, give a warning
@@ -37,9 +43,9 @@ Type shutdown -c in a terminal to cancel."
 			break #pending=false before while loop
 		fi
 	done
-	if $pending && [ $(ps -aux | grep -c 'Plex Transcoder') -gt $proc_num ] ; then #if shutdown is pending and plex start
+	if $pending && [ $(ps -aux | grep -c 'plexmediaserver') -gt $proc_num ] ; then #if shutdown is pending and plex start
   		echo [$(date)] 'Counter:' $counter >> $log_path
-		echo [$(date)] 'Plex Processes:' $(ps -aux | grep -c 'Plex Transcoder') >> $log_path
+		echo [$(date)] 'Plex Processes:' $(ps -aux | grep -c 'plexmediaserver') >> $log_path
 		echo [$(date)] 'Shutdown canceled due Plex activity. Restarting timer...' >> $log_path
 		sudo /sbin/shutdown -c
 		notify-send -t 600000 -i "/usr/share/plex-shutdown/plex.svg" "Shutdown canceled due Plex activity."
