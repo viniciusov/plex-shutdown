df66a450f9cb7e557acf13be984f957c843ba0ce
Vinícius Orsi Valente <viniciusov@hotmail.com>
2019-07-09 18:36:40 -0300

    Tabs corrected

--- plex-shutdown.sh
+++ plex-shutdown.sh
@@ -25,9 +25,9 @@ while : ; do
 	pending=false
 	
 	upload1=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
-    sleep 10s
-    upload2=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
-    upload=$((($upload2 - $upload1)/10)) #check upload rate (average)
+	sleep 10s
+	upload2=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
+	upload=$((($upload2 - $upload1)/10)) #check upload rate (average)
 
 	while [ $(ps -aux | grep -c 'plexmediaserver') -le $proc_num ] && [ $upload -le $up_trigger ] ; do #if plex is not running
 
@@ -35,9 +35,9 @@ while : ; do
 		((counter++))
 
 		upload1=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
-        sleep 10s
-        upload2=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
-        upload=$((($upload2 - $upload1)/10)) #check upload rate (average)
+		sleep 10s
+		upload2=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
+		upload=$((($upload2 - $upload1)/10)) #check upload rate (average)
 
 		if [ $counter -lt $idle_time ] && [ $(ps -aux | grep -c 'plexmediaserver') -gt $proc_num ] ; then #if plex start
 			echo [$(date)] 'Counter:' $counter >> $log_path
@@ -45,10 +45,10 @@ while : ; do
 			echo [$(date)] 'Plex activity detected. Restarting timer...' >> $log_path
 			break		
 		elif [ $counter -lt $idle_time ] && [ $upload -gt $up_trigger ] ; then
-            echo [$(date)] 'Counter:' $counter >> $log_path
-            echo [$(date)] 'Current upload rate (bytes/s):' $upload >> $log_path
-            echo [$(date)] 'Uploading content. Restarting timer...' >> $log_path
-            break
+			echo [$(date)] 'Counter:' $counter >> $log_path
+			echo [$(date)] 'Current upload rate (bytes/s):' $upload >> $log_path
+			echo [$(date)] 'Uploading content. Restarting timer...' >> $log_path
+			break
 		elif [ $counter -eq $idle_time ] ; then #if not running until 'idle_time', give a warning
 			shut_time=$(date --date='10 minutes' +"%T")
 			notify-send -t $(($warning_time*60*1000)) -i "/usr/share/plex-shutdown/plex.svg" "WARNING:
@@ -71,20 +71,20 @@ Type shutdown -c in a terminal to cancel."
 	if $pending ; then
 
 		upload1=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
-    	sleep 10s
-    	upload2=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
-    	upload=$((($upload2 - $upload1)/10)) #check upload rate (average)
+		sleep 10s
+		upload2=$(cat /proc/net/dev | grep $interface | awk '{print $2}')
+		upload=$((($upload2 - $upload1)/10)) #check upload rate (average)
 
 		if [ $(ps -aux | grep -c 'plexmediaserver') -gt $proc_num ] ; then #if shutdown is pending and plex start
-  			echo [$(date)] 'Counter:' $counter >> $log_path
+			echo [$(date)] 'Counter:' $counter >> $log_path
 			echo [$(date)] 'Plex Processes:' $(ps -aux | grep -c 'plexmediaserver') >> $log_path
 			echo [$(date)] 'Shutdown canceled due Plex activity. Restarting timer...' >> $log_path
 			sudo /sbin/shutdown -c
 			notify-send -t $(($warning_time*60*1000)) -i "/usr/share/plex-shutdown/plex.svg" "Shutdown canceled."
-    	elif [ $upload -gt $up_tigger ] ; then
-        	echo [$(date)] 'Counter:' $counter >> $log_path
-        	echo [$(date)] 'Current upload rate (bytes/s):' $upload >> $log_path
-        	echo [$(date)] 'Shutdown canclede due upload. Restarting timer...' >> $log_path
+		elif [ $upload -gt $up_tigger ] ; then
+			echo [$(date)] 'Counter:' $counter >> $log_path
+			echo [$(date)] 'Current upload rate (bytes/s):' $upload >> $log_path
+			echo [$(date)] 'Shutdown canclede due upload. Restarting timer...' >> $log_path
 			sudo /sbin/shutdown -c
 			notify-send -t $(($warning_time*60*1000)) -i "/usr/share/plex-shutdown/plex.svg" "Shutdown canceled."
 		fi 
